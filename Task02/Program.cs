98254c792a53a39aefaa54be4537cd464862a7fe
Ilya Ustinov <ustin_1999@mail.ru>
2023-03-01 13:23:22 +0300

    Added Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numB = Convert.ToInt32(Console.ReadLine());
+if(numA>numB)
+{
+    Console.WriteLine($"Наибольшее число -> {numA}");
+}
+else
+{
+    Console.WriteLine($"Наибольшее число -> {numB}");
+}
