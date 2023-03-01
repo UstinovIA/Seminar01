b232bbf1ba93b1993bcdc34e84deed35c4451d1c
Ilya Ustinov <ustin_1999@mail.ru>
2023-03-01 13:48:27 +0300

    Added Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число не является четным");
+}
