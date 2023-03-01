291976e361fb6d3e9b3a83094453fc1b465ca3c8
Ilya Ustinov <ustin_1999@mail.ru>
2023-03-01 14:04:20 +0300

    Added Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите натуральное число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 0)
+{
+    Console.Write($"{num} -> ");
+    int count = 2;
+    while (num >= count)
+    {
+        Console.Write($"{count} ");
+        count += 2;
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели не натуральное число");
+}
